e71ceb7aef0d6e71f99bbee13c2e9c8ba2995c43
Racklyn <racklynsotero@gmail.com>
2022-07-25 00:23:26 -0300

    First commit

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            home: undefined;
+            new: undefined;
+            details: { orderId: string } //Tipo de parâmetro que será passado para abrir a tela
+        }
+    }
+}
